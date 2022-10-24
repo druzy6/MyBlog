e69741d06b12c54c340bcb10c6e5a043d1db60ea
druzy6 <roysharon6@gmail.com>
2022-10-24 11:16:43 +0300

    get_it_over

--- client/src/PostMaker.js
+++ client/src/PostMaker.js
@@ -21,7 +21,17 @@ function PostMaker(){
   }
 
   function picsAdd(event){
-    setPics(event.target.value);
+    setPics(event.target.value[0]);
+  }
+
+  function fileUpload(event){
+    const formData = new FormData();
+    console.log(pics);
+    // pics.map(pic => {
+    //   console.log(pic);
+    //   formData.append('image', pic)
+    //   formData.append('name', )
+    // });
   }
 
   function formSub(event){
@@ -42,8 +52,9 @@ function PostMaker(){
     <div>
       <Form>
         <Form.Group controlId="formFileMultiple" className="mb-3">
-          <Form.Label>post pictures</Form.Label>
+          <Form.Label>upload pictures</Form.Label>
           <Form.Control type="file" value={pics} onChange={picsAdd} multiple />
+          <Button variant="primary" onClick={fileUpload}> upload </ Button>
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>char count: {chars.numOfLetters}</Form.Label>
